b637fcfd8eae028d9a571f44f891943800907f7c
KOMMAHRUSHIKESHAWARREDDY <kommahrushikeshwarreddy@gmail.com>
2025-12-30 23:50:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,14 +6,14 @@ import seaborn as sns
 st.set_page_config(page_title="EDA Project", layout="wide")
 st.title("Exploratory Data Analysis")
 
-# Load dataset
-df = pd.read_csv("dataset.csv")
+# Load Excel dataset
+df = pd.read_excel("online_course_recommendation.xlsx")
 
 st.subheader("Dataset Preview")
 st.dataframe(df.head())
 
 st.subheader("Summary Statistics")
-st.write(df.describe())
+st.write(df.describe(include="all"))
 
 st.subheader("Missing Values")
 st.write(df.isnull().sum())
@@ -29,5 +28,5 @@ st.pyplot(fig)
 st.subheader("Feature Distribution")
 col = st.selectbox("Select Column", numeric_df.columns)
 fig2, ax2 = plt.subplots()
-sns.histplot(df[col], kde=True, ax=ax2)
+sns.histplot(numeric_df[col], kde=True, ax=ax2)
 st.pyplot(fig2)
